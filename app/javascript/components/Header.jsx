9c133f37ce5226e40324ad1e5cee549b04c8c09f
SantillanaJasmin <jajasantillana@gmail.com>
2021-10-10 23:12:35 +0800

    Refactor code

--- app/javascript/components/Header.jsx
+++ app/javascript/components/Header.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Layout, Menu } from "antd";
 
 const { Header } = Layout;
+const { Item } = Menu;
 
 export default () => (
   <Header>
     <div className="logo" />
     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-      <Menu.Item key="1">Home</Menu.Item>
-      <Menu.Item key="2">Our Services</Menu.Item>
-      <Menu.Item key="3">Contact</Menu.Item>
+      <Item key="1">Home</Item>
+      <Item key="2">Our Service</Item>
+      <Item key="3">Contact</Item>
     </Menu>
   </Header>
 );
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,7 +4,7 @@
 
 import React from "react";
 import { render } from "react-dom";
-import App from "../components/App";
+import App from "../components/index";
 
 document.addEventListener("DOMContentLoaded", () => {
   render(<App />, document.body.appendChild(document.createElement("div")));
